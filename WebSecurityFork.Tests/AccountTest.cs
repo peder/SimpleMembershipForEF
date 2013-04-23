c58faffd0778e240ceb00d404aff182deacb2c4d
riceweb <rice04@gmail.com>
2013-04-23 12:58:47 -0500

    Trying to get WebSecurityFork.Tests project to work as an integration test project, but it can't connect to a local MDF database

---
+++ WebSecurityFork.Tests/AccountTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebMatrix.WebData;
+
+namespace WebSecurityFork.Tests
+{
+	[TestClass]
+	public class Account
+	{
+		[TestClass]
+		public class Create
+		{
+			[TestInitialize]
+			public void Init()
+			{
+				if (!WebMatrix.WebData.WebSecurity.Initialized)
+					WebMatrix.WebData.WebSecurity.InitializeDatabaseConnection("DefaultConnectionMDF",
+						"UserProfile", "UserId", "UserName", autoCreateTables: true);
+			}
+
+			[TestMethod]
+			public void CanCreateAccount()
+			{
+				var simpleMembershipProvider = new SimpleMembershipProvider();
+
+				simpleMembershipProvider.CreateAccount("Test1", "notAPassword1");
+			}
+
+			[TestMethod]
+			public void CanDeleteAccount()
+			{
+				var simpleMembershipProvider = new SimpleMembershipProvider();
+
+				simpleMembershipProvider.DeleteAccount("Test1");
+			}
+		}
+	}
+}
